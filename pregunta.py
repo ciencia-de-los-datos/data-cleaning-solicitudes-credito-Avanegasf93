cebeaec8a743a41cdfd89b49c1cce1f3cc68e0d7
Avanegasf93 <112149548+Avanegasf93@users.noreply.github.com>
2022-10-28 04:29:31 -0500

    Commit C
    Commit C

--- pregunta.py
+++ pregunta.py
@@ -8,42 +8,31 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
+
 def clean_data():
+
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df.drop(['Unnamed: 0'], axis=1,inplace=True)
+    df.dropna(inplace=True)
     
-   df = pd.read_csv("solicitudes_credito.csv", sep=";")
-   df.rename(columns = {'Unnamed: 0': 'index'}, inplace = True)
-   df.set_index('index', inplace = True)
+    df["sexo"] = df["sexo"].apply(lambda x: str(x).lower().strip())
 
-   df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x: str(x).lower().strip())
 
-   df.sexo = df.sexo.str.lower()
-   df.sexo = df.sexo.strip()
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).lower().strip())
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).replace("-"," ").replace("_"," ").strip())
 
-   df.idea_negocio = df.idea_negocio.str.lower()
-   df.idea_negocio = df.idea_negocio.str.strip('_')
-   df.idea_negocio = df.idea_negocio.str.strip('-')
-   df.idea_negocio = df.idea_negocio.str.strip()
-   df.idea_negocio = df.idea_negocio.str.replace("_", "-")
-   df.idea_negocio = df.idea_negocio.str.replace("-", " ")
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).lower().strip())
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
 
-   df.monto_del_credito = df.monto_del_credito.str.replace('$','')
-   df.monto_del_credito = df.monto_del_credito.str.replace(',','')
-   df.monto_del_credito = df.monto_del_credito.astype(float)
+    df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().replace("_"," ").replace("-"," "))
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().strip())
 
-   df.barrio = df.barrio.str.lower()
-   df.barrio = df.barrio.str.strip()
-   df.barrio = df.barrio.str.replace("_", "-")
-   df.barrio = df.barrio.str.replace("-", " ")
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
 
-   df.línea_credito = df.línea_credito.str.lower()
-   df.línea_credito = df.línea_credito.str.strip('_')
-   df.línea_credito = df.línea_credito.str.strip('-')
-   df.línea_credito = df.línea_credito.str.strip()
-   df.línea_credito = df.línea_credito.str.replace("_", "-")
-   df.línea_credito = df.línea_credito.str.replace("-", " ")
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
+    
+    df.drop_duplicates(inplace=True)
 
-   df.dropna(inplace=True)
-   df.drop_duplicates(inplace=True)
-
-
-   return df
+    
+    return df
