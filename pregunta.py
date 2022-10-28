654ca7587a87c97a46c4be03d7925f799b8a20ef
Avanegasf93 <112149548+Avanegasf93@users.noreply.github.com>
2022-10-28 02:19:09 -0500

    Commit 5
    Commit 5

--- pregunta.py
+++ pregunta.py
@@ -6,41 +6,43 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-import pandas as pd
-from dataclasses import replace
-
-
-def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+df = pd.read_csv("solicitudes_credito.csv", sep=";")
     df = df[df.columns[1:]]
     df.drop_duplicates(inplace=True)
+    #df.drop(['Unnamed: 0'], axis=1,inplace=True)
     df.dropna(inplace=True)
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'])
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
 
 
     df['sexo'] = df.sexo.drop_duplicates()
-    df['sexo'] = df.sexo.str.lower()
+    df['sexo'] = df.sexo.str.lower().strip()
+
+    df['tipo_de_emprendimiento'] = df.tipo_de_emprendimiento.drop_duplicates()
+    df['tipo_de_emprendimiento'] = df.tipo_de_emprendimiento.str.lower()
+    df['tipo_de_emprendimiento'] = df.tipo_de_emprendimiento.str.strip()
 
     df['idea_negocio'] = df.idea_negocio.drop_duplicates()
-    df['idea_negocio'] = df.idea_negocio.str.replace(" ", "_", regex=True)
-    df['idea_negocio'] = df.idea_negocio.str.replace("-", "_", regex=True)
+    df['idea_negocio'] = df.idea_negocio.str.replace("_", " ", regex=True)
+    df['idea_negocio'] = df.idea_negocio.str.replace("-", " ", regex=True)
     df['idea_negocio'] = df.idea_negocio.str.lower()
     df['idea_negocio'] = df.idea_negocio.str.strip()
-
-    df['estrato'] = df.estrato.drop_duplicates()
-
-    df['comuna_ciudadano'] = df.comuna_ciudadano.drop_duplicates()
 
     df['monto_del_credito'] = df.monto_del_credito.drop_duplicates()
     df['monto_del_credito'] = df.monto_del_credito.str.replace(" ", "", regex=True)
     df['monto_del_credito'] = df.monto_del_credito.str.replace(",", "", regex=True)
     df['monto_del_credito'] = df.monto_del_credito.str.replace("$", "", regex=True)
+    df['monto_del_credito'] = df.monto_del_credito.str.replace(".00", "", regex=True)
     df['monto_del_credito'] = df.monto_del_credito.str.strip()
 
+    df['barrio'] = df.barrio.drop_duplicates()
+    df['barrio'] = df.barrio.str.replace("_", " ", regex=True)
+    df['barrio'] = df.barrio.str.replace("-", " ", regex=True)
+    df['barrio'] = df.barrio.str.lower()
+    df['barrio'] = df.barrio.str.strip()
+
     df['línea_credito'] = df.línea_credito.drop_duplicates()
-    df['línea_credito'] = df.línea_credito.str.replace(" ", "_", regex=True)
-    df['línea_credito'] = df.línea_credito.str.replace("-", "_", regex=True)
+    df['línea_credito'] = df.línea_credito.str.replace("_", " ", regex=True)
+    df['línea_credito'] = df.línea_credito.str.replace("-", " ", regex=True)
     df['línea_credito'] = df.línea_credito.str.replace('soli diaria','solidaria', regex=True)
     df['línea_credito'] = df.línea_credito.str.lower()
     df['línea_credito'] = df.línea_credito.str.strip()
@@ -51,5 +53,5 @@ def clean_data():
 
 
     df.drop_duplicates(inplace = True)
-
+    print(df.sexo.value_counts())
     return df
