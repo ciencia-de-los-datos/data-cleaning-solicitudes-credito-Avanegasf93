bb0ef323186e754c6f41515d020a80852832b880
Avanegasf93 <112149548+Avanegasf93@users.noreply.github.com>
2022-10-28 04:13:57 -0500

    Commit 9
    Commit 9

--- pregunta.py
+++ pregunta.py
@@ -10,42 +10,39 @@ import pandas as pd
 
 def clean_data():
     
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.drop_duplicates(inplace=True)
-    df.dropna(inplace=True)
+   df = pd.read_csv("solicitudes_credito.csv", sep=";")
+   df.drop(['Unnamed: 0'], axis=1,inplace=True)
+
+   df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
+
+   df['sexo'] = df.sexo.str.lower()
+   df['sexo'] = df.sex
+
+   df['idea_negocio'] = df.idea_negocio.str.lower()
+   df['idea_negocio'] = df.idea_negocio.str.strip('_')
+   df['idea_negocio'] = df.idea_negocio.str.strip('-')
+   df['idea_negocio'] = df.idea_negocio.str.strip()
+   df['idea_negocio'] = df.idea_negocio.str.replace("_", "-")
+   df['idea_negocio'] = df.idea_negocio.str.replace("-", " ")
+
+   df['monto_del_credito'] = df.monto_del_credito.str.replace('$','')
+   df['monto_del_credito'] = df.monto_del_credito.str.replace(',','')
+   df.monto_del_credito = df.monto_del_credito.astype(float)
+
+   df['barrio'] = df.barrio.str.lower()
+   df['barrio'] = df.barrio.str.strip()
+   df['barrio'] = df.barrio.str.replace("_", "-")
+   df['barrio'] = df.barrio.str.replace("-", " ")
+
+   df['línea_credito'] = df.línea_credito.str.lower()
+   df['línea_credito'] = df.línea_credito.str.strip('_')
+   df['línea_credito'] = df.línea_credito.str.strip('-')
+   df['línea_credito'] = df.línea_credito.str.strip()
+   df['línea_credito'] = df.línea_credito.str.replace("_", "-")
+   df['línea_credito'] = df.línea_credito.str.replace("-", " ")
+
+   df.dropna(inplace=True)
+   df.drop_duplicates(inplace=True)
 
 
-    df['sexo'] = df.sexo.str.lower()
-    df['sexo'] = df.sexo.str.strip()
-
-    df['tipo_de_emprendimiento'] = df.tipo_de_emprendimiento.str.lower()
-    df['tipo_de_emprendimiento'] = df.tipo_de_emprendimiento.str.strip()
-
-    df['idea_negocio'] = df.idea_negocio.str.replace("_", " ", regex=True)
-    df['idea_negocio'] = df.idea_negocio.str.replace("-", " ", regex=True)
-    df['idea_negocio'] = df.idea_negocio.str.lower()
-    df['idea_negocio'] = df.idea_negocio.str.strip()
-
-    df['monto_del_credito'] = df.monto_del_credito.str.replace(" ", "", regex=True)
-    df['monto_del_credito'] = df.monto_del_credito.str.replace(",", "", regex=True)
-    df['monto_del_credito'] = df.monto_del_credito.str.replace("$", "", regex=True)
-    df['monto_del_credito'] = df.monto_del_credito.str.replace(".00", "", regex=True)
-    df['monto_del_credito'] = df.monto_del_credito.str.strip()
-
-    df['barrio'] = df.barrio.str.replace("_", " ", regex=True)
-    df['barrio'] = df.barrio.str.replace("-", " ", regex=True)
-    df['barrio'] = df.barrio.str.lower()
-    df['barrio'] = df.barrio.str.strip()
-
-    df['línea_credito'] = df.línea_credito.str.replace("_", " ", regex=True)
-    df['línea_credito'] = df.línea_credito.str.replace("-", " ", regex=True)
-    df['línea_credito'] = df.línea_credito.str.replace('soli diaria','solidaria', regex=True)
-    df['línea_credito'] = df.línea_credito.str.lower()
-    df['línea_credito'] = df.línea_credito.str.strip()
-
-    df['barrio'] = df['barrio'].astype(str)
-    df['línea_credito'] = df['línea_credito'].astype(str)
-
-    df.drop_duplicates(inplace = True)
-
-    return df
+   return df
